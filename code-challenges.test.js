6b734d385c8c7bdc827d73a99c4c74df06114a0f
RyanJ2463 <jacobellr2017@gmail.comjacobellr2017@gmail.comcd>
2022-03-13 21:22:05 -0400

    finished assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,36 +14,110 @@
 // --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
 
 // a) Create a test with an expect statement using the variable provided. HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
+// a describe method that lists the name of the function OR naming of the particular test.
+describe("shuffle", () => {
 
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
-// Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
-// Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+    // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+    it("takes in an array, removes the first item from the array and shuffles the remaining content", () => {
+        const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+        // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+        const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+        // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+  
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(shuffle(colors1)).toEqual(expect.arrayContaining(["yellow", "blue", "pink", "green"]))
+      expect(shuffle(colors2)).toEqual(expect.arrayContaining(["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]))
+    })
+  })
 
+//   create a function shuffle
+//   function takes in array 
+//   slice to remove the first value of the array
+//   use math.random to randomize the array
 
-// b) Create the function that makes the test pass.
+ 
+  const shuffle = (array) => {
+      return array.slice(0).sort(() => Math.random() - 0.5)
+  }
+
+// ● shuffle › takes in an array, removes the first item from the array and shuffles the remaining content
+// ReferenceError: shuffle is not defined
 
 
 // --------------------2) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const nums1 = [3, 56, 90, -8, 0, 23, 6]
-// Expected output: [-8, 90]
-const nums2 = [109, 5, 9, 67, 8, 24]
-// Expected output: [5, 109]
+describe("minMax", () => {
 
+    // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+    it("that takes an array of numbers and returns an array of the minimum and maximum numbers in that order", () => {
+        const nums1 = [3, 56, 90, -8, 0, 23, 6]
+        // Expected output: [-8, 90]
+        const nums2 = [109, 5, 9, 67, 8, 24]
+        // Expected output: [5, 109]
+  
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(minMax(nums1)).toEqual(expect.arrayContaining([-8, 90]))
+      expect(minMax(nums2)).toEqual(expect.arrayContaining([5, 109]))
+    })
+  })
+     
+//   ● minMax › that takes an array of numbers and returns an array of the minimum and maximum numbers in that order
+
+//   ReferenceError: minMax is not defined
+
+// create a function minMax
+// set 2 new variables equal to the max and min with spread opperator
+// return the 2 vars in an array
 
 // b) Create the function that makes the test pass.
-
+const minMax = (array) => {
+  let minArr = Math.min(...array)
+  let maxArr = Math.max(...array)
+  return [minArr, maxArr]
+}
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
+describe("noDuplicate", () => {
 
-const testArray1 = [3, 7, 10, 5, 4, 3, 3]
-const testArray2 = [7, 8, 2, 3, 1, 5, 4]
-// Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+    // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+    it("that takes an artakes in two arrays as arguments and returns one array with no duplicate valuesray of numbers and returns an array of the minimum and maximum numbers in that order", () => {
+
+        const testArray1 = [3, 7, 10, 5, 4, 3, 3]
+        const testArray2 = [7, 8, 2, 3, 1, 5, 4]
+        // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+  
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(noDuplicate(testArray1, testArray2)).toEqual(expect.arrayContaining([3, 7, 10, 5, 4, 8, 2, 1]))
+    })
+  })
+
+//   ● noDuplicate › that takes an artakes in two arrays as arguments and returns one array with no duplicate valuesray of numbers and returns an array of the minimum and maximum numbers in that order
+
+//   ReferenceError: noDuplicate is not defined
 
 
 // b) Create the function that makes the test pass.
+// create a function that takes in 2 arrays
+// set a new array equal to both arrays
+// use new set to remove duplicates 
+// return the new array
+
+const noDuplicate = (array1, array2) => {
+    let newArr = [...new Set([...array1,...array2])]
+    return newArr
+}
+
+// i tried without using new Set and still recieved a passing.
+// i tested the code and looked over the notes but from my understanding the ... wont remove duplicates but still passed the test.
+
+// const noDuplicate = (array1, array2) => {
+//     let newArr = [...array1,...array2]
+//     return newArr
+// }
+
+
+
--- interview-questions.md
+++ interview-questions.md
@@ -7,52 +7,52 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What are props in React?
 
-  Your answer:
+  Your answer: In my experience props in React is a way to transfer data from the main class to a method in a different file. Props is a read only so the method in the other file can not change the information.
 
-  Researched answer:
+  Researched answer: Props in React is a way to pass information from one component to another. Props come from state and is used to pass information to other components that can display information or let an user interact with that information.
 
 
 
 2. What is a DOM event?
 
-  Your answer:
+  Your answer: In my experience a DOM event would be adding a button to your html file and using javascript to have the button change something by a user clicking the button. 
 
-  Researched answer:
+  Researched answer: The DOM event is a way to execute code when a user clicks on an html element. By adding Javascript code the webpage can do something when a user interacts. The event will be when the user clicks a button which triggers the javascript code to run. not before.
 
 
 
 3. What is object-oriented programming? How is it different than functional programming?
 
-  Your answer:
+  Your answer: In my experience object-oriented programming is a way to element using a bunch of variables and methods like functional programming. Insteaad of creating variables we create a object to store data and a function inside that way we can make code more dynamic and easier to change the data.
 
-  Researched answer:
+  Researched answer: Functional programming attempts to avoid changing state and data. the output is always the same. OOP holds data in objects and can be manipulated through methods or functions.
 
 
 
 4. What is the difference between a Float and an Integer in Ruby?
 
-  Your answer:
+  Your answer: In my experience a float is a number with deciemls while an integer is a whole number.
 
-  Researched answer:
+  Researched answer: In ruby an interger is a number with no decimals while a float is a number with decimals 
 
 
 
 5. Ruby has an implicit return. What does that mean?
 
-  Your answer:
+  Your answer: In my experience ruby does not need to be told to return after a method or conditional like JavaScript. 
 
-  Researched answer:
+  Researched answer: When a method is ran without a return ruby will return the value of the last instruction in the method.
 
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Instance Variable:
+1. Instance Variable: an instance variable is a type of variable which starts with an @ symbol
 
-2. PostgreSQL:
+2. PostgreSQL: is an open source object-relational database management system, which means Postgres looks at databases through an object-oriented programming lens
 
-3. Ruby on Rails:
+3. Ruby on Rails: is a application framework written in ruby that runs on the server-side
 
-4. ORM:
+4. ORM: is a programming technique for converting data between incompatible systems using object-oriented programming languages. 
 
-5. Active Record:
+5. Active Record: is the interface between a Rails application and the database
